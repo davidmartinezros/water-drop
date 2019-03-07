1fdff65b1956a439e10ff2707fc7e6ec3aaaa9c2
David Martinez Ros <davidnezan@gmail.com>
2019-03-07 21:07:36 +0100

    make the necessary imports and define a plane and a water sphere on the scene

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, ViewChild, ElementRef} from '@angular/core';
-import * as THREE from 'three';
+import THREE from './js/three';
 
 @Component({
     selector: 'app-root',
@@ -7,24 +7,45 @@ import * as THREE from 'three';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    /**
+     * hacer una surface ondulada donde las gotas de agua van de agujero en agujero
+     * el usuario puede mover el plano y hacer mover las gotas de agua entre los agujeros de la surface
+     */
     @ViewChild('rendererContainer') rendererContainer: ElementRef;
 
     renderer = new THREE.WebGLRenderer();
     scene = null;
     camera = null;
-    mesh = null;
+    plane = null;
+    waterDrop = null;
+    controls: THREE.OrbitControls;
 
     constructor() {
         this.scene = new THREE.Scene();
 
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-        this.camera.position.z = 1000;
+        this.camera.position.z = 100;
+        this.camera.position.y = -100;
 
-        const geometry = new THREE.BoxGeometry(200, 200, 200);
+        this.initOptionsEvents();
+
+        const geometryPlane = new THREE.PlaneBufferGeometry(300, 300, 30, 30);
+        const geometrySphere = new THREE.SphereGeometry(10, 64, 64);
+        this.waterDrop = new THREE.Water(geometrySphere);
         const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-        this.mesh = new THREE.Mesh(geometry, material);
+        this.plane = new THREE.Mesh(geometryPlane, material);
 
-        this.scene.add(this.mesh);
+        this.scene.add(this.plane);
+        this.scene.add(this.waterDrop);
+    }
+
+    private initOptionsEvents() {
+      this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
+      //this.controls.maxPolarAngle = Math.PI * 0.495;
+      //this.controls.target.set( 0, 0, 0 );
+      this.controls.enablePan = true;
+      this.controls.minDistance = 40.0;
+      this.controls.maxDistance = 200.0;
     }
 
     ngAfterViewInit() {
@@ -35,8 +56,8 @@ export class AppComponent {
 
     animate() {
         window.requestAnimationFrame(() => this.animate());
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y += 0.02;
+        //this.mesh.rotation.x += 0.01;
+        //this.mesh.rotation.y += 0.02;
         this.renderer.render(this.scene, this.camera);
     }
 }
---
+++ src/app/js/three.js
@@ -0,0 +1,6 @@
+import * as THREE from 'three'; // build/three.js from node_module/three
+window.THREE = THREE;
+require('three/examples/js/controls/OrbitControls.js');
+require('three/examples/js/objects/Water.js');
+
+export default THREE;
