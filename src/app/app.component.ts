aa0f759d23eebb1a95a3919d19a86adf5b9da11e
David Martinez Ros <davidnezan@gmail.com>
2019-03-07 19:29:36 +0100

    first commit with three.js installed at AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'water-drop-tmp';
+    @ViewChild('rendererContainer') rendererContainer: ElementRef;
+
+    renderer = new THREE.WebGLRenderer();
+    scene = null;
+    camera = null;
+    mesh = null;
+
+    constructor() {
+        this.scene = new THREE.Scene();
+
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        this.camera.position.z = 1000;
+
+        const geometry = new THREE.BoxGeometry(200, 200, 200);
+        const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+        this.mesh = new THREE.Mesh(geometry, material);
+
+        this.scene.add(this.mesh);
+    }
+
+    ngAfterViewInit() {
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+        this.animate();
+    }
+
+    animate() {
+        window.requestAnimationFrame(() => this.animate());
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        this.renderer.render(this.scene, this.camera);
+    }
 }
